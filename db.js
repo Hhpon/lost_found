9480e21cdf661f354f7adf85ae88315e169b4141
Hhpon <624791164@qq.com>
2019-05-15 21:16:29 +0800

    change mongodb port

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
 let mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/database2', { useNewUrlParser: true })
+
+let env = process.env.NODE_ENV || 'development'
+let dbUrl = 'mongodb://127.0.0.1:20811/database2'
+// 开发环境连接测试使用的 MongoDB 服务器
+if (env === 'development') {
+    dbUrl = 'mongodb://127.0.0.1/database2'
+}
+
+mongoose.connect(dbUrl, { useNewUrlParser: true })
+
 const base2Schema = new mongoose.Schema({
     time:String,
     touxiang:String,
